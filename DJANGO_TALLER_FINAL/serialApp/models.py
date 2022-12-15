9fc3e156085b50285f98a0bed5898003a7a7d33d
José Velásquez <joseivecas@gmail.com>
2022-12-14 23:56:11 -0300

    html participantes
    str institucion

--- DJANGO_TALLER_FINAL/serialApp/models.py
+++ DJANGO_TALLER_FINAL/serialApp/models.py
@@ -13,6 +13,9 @@ class Institucion(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre
+
 class Participante(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
